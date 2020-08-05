d51cec8d4b41310eb1e5a13b34a3456ccba65870
Justin Lonh <justinlonh.cs@gmail.com>
2020-08-04 20:08:43 -0700

    GameController update
    added some timing functions in game controller

--- Lotto Tree/Assets/Scripts/GameController.cs
+++ Lotto Tree/Assets/Scripts/GameController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+
+    private int[] currentDraw;
+    private float timeLeft;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentDraw = new int[6];
+        timeLeft = 240.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeLeft <= 0){
+            timeLeft = 240.0f;
+            for (int i = 0; i < 6; i++){
+                currentDraw[i] = Random.Range(1, 82);
+            }
+        }
+
+        if (timeLeft <= 120){
+            //to-do: leaf falling
+            foreach(GameObject leaf in GameObject.FindGameObjectsWithTag("Leaf")){
+                leaf.transform.Translate(0, Time.deltaTime, 0);
+            }
+        }
+
+        timeLeft -= Time.deltaTime;
+
     }
 }
