c7632cb01fac4e2658bc1de6d605e5783d0a4111
Justin Lonh <justinlonh.cs@gmail.com>
2020-08-04 23:45:48 -0700

    fixed timer
    changed TextMeshProUGUI timer to TextMeshPro to allow for timer to appear in game

--- Lotto Tree/Assets/Scripts/Timer.cs
+++ Lotto Tree/Assets/Scripts/Timer.cs
@@ -7,20 +7,22 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     private float timeLeft;
-    private TextMeshProUGUI textmeshPro;
+    private TextMeshPro textmeshPro;
 
     // Start is called before the first frame update
     void Start()
     {
         timeLeft = 240.0f;
-        textmeshPro = GetComponent<TextMeshProUGUI>();
+        textmeshPro = GetComponent<TextMeshPro>();
     }
 
     // Update is called once per frame
     void Update()
     {
         timeLeft -= Time.deltaTime;
-        textmeshPro.SetText("Something");
-        // textmeshPro.SetText("{0}",timeLeft);
+        textmeshPro.SetText("{0}:{1}",(int)(timeLeft/60), (int)(timeLeft%60));
+        if (timeLeft <= 0){
+            timeLeft = 240.0f;
+        }
     }
 }
