1edb735496eb67b57e6f36ae629e26c0071f4010
Justin Lonh <justinlonh.cs@gmail.com>
2020-08-09 19:58:24 -0700

    Revert "Revert "Add box prefabs""
    This reverts commit 94c3a4c96d9acfb471db84a69758c3197aa45d21.

---
+++ Lotto Tree/Assets/Scripts/BoxLabel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class BoxLabel : MonoBehaviour
+{
+
+    private TextMeshPro boxLabel;
+    private int boxTotal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxLabel = GetComponent<TextMeshPro>();
+        boxTotal = 0;
+        IncrementLabel(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void IncrementLabel(int i)
+    {
+        boxTotal += i;
+        boxLabel.SetText(String.Format("{0}", boxTotal));
+        boxLabel.ForceMeshUpdate(true);
+    }
+}
